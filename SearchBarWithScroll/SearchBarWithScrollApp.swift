f6e009d1a84cbc306105c5618ef536bf67e5a1fe
TransloadApp <transloadapp@gmail.com>
2024-12-04 10:45:20 +0700

    Initial Commit

---
+++ SearchBarWithScroll/SearchBarWithScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarWithScrollApp.swift
+//  SearchBarWithScroll
+//
+//  Created by Nguyen Trong Thach on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarWithScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
